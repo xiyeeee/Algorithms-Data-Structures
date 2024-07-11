bc1c06b8ab032da23663d11aa1513719b3991afc
Xiyeeee <1209401586@qq.com>
2024-07-12 01:43:23 +0800

    单链表获取节点下标 ，以及元素 ， 以及错误抛出

--- src/main/java/cn/iocoder/boot/array/linkList/SingleLinkedList.java
+++ src/main/java/cn/iocoder/boot/array/linkList/SingleLinkedList.java
@@ -1,36 +1,35 @@
 package cn.iocoder.boot.array.linkList;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.Iterator;
-import java.util.Spliterator;
 import java.util.function.Consumer;
 
 /**
  * @Description
  * @Author xiye
  * @Date 2024/7/11
- *
+ * <p>
  * guan
  */
 /*
  关于类中 什么时候加static , 经验 , 如果一个一个静态类中与外部的成员变量
  那他就不能使用static修饰
  * */
-public class SingleLinkedList  implements Iterable<Integer>{
-/**
- *
- */
+public class SingleLinkedList implements Iterable<Integer> {
+    /**
+     *
+     */
     private Node head;
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
             Node p = head;
+
             @Override
             public boolean hasNext() {
-                return p!=null;
+                return p != null;
             }
 
             @Override
@@ -43,35 +42,91 @@ public class SingleLinkedList  implements Iterable<Integer>{
     }
 
 
-
     @AllArgsConstructor
-    private static class Node{
+    private static class Node {
         int value;
         Node next;
+
         @Override
         public String toString() {
             return "Node{" +
                     "value=" + value +
                     '}';
         }
-     }
-     // 头插法
-    public void addFirst(int value){
+    }
+
+    // 头插法
+    public void addFirst(int value) {
         head = new Node(value, head);
     }
-    public  void  loop1(Consumer<Integer> consumer){
+
+    public void loop1(Consumer<Integer> consumer) {
         Node p = head;
-        while (p!=null){
+        while (p != null) {
             consumer.accept(p.value);
             p = p.next;
+        }
+    }
+
+    public void loop2(Consumer<Integer> consumer) {
+        for (Node p = head; p != null; p = p.next) {
+            consumer.accept(p.value);
 
         }
     }
-    public  void  loop2(Consumer<Integer> consumer){
-        for ( Node p = head; p!=null ; p = p.next) {
-            consumer.accept(p.value);
 
+    /**
+     * @Description 尾插法先找到最后一个节点
+     * @Author xiye
+     * @Date
+     */
+    private Node findLast() {
+        if (head == null) return null;
+        Node p;
+        for (p = head; p.next != null; p = p.next) {
         }
+        return p;
+    }
+
+    /**
+     * @Description 尾插法
+     * @Author xiye
+     * @Date
+     */
+    public void addLast(int value) {
+        Node last = findLast();
+        if (last == null) {
+            addFirst(value);
+            return;
+        }
+        last.next = new Node(value, null);
+    }
+
+    /**
+     * @Description
+     * @Author xiye
+     * @Date
+     */
+    private Node getNode(int index) {
+        int i = 0;
+        for (Node p = head; p.next != null; p = p.next, i++) {
+            if (i == index) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public int getNodeValue(int index) {
+
+        Node node = getNode(index);
+        System.out.println(node);
+        if (node == null) {
+            throw new IllegalArgumentException(
+                    String.format("index [%d] 不合法 %n ", index)
+            );
+        }
+        return node.value;
     }
 
 }
--- src/test/java/cn/iocoder/boot/linkList/TestSingleLinkedList.java
+++ src/test/java/cn/iocoder/boot/linkList/TestSingleLinkedList.java
@@ -2,6 +2,9 @@ package cn.iocoder.boot.linkList;
 
 import cn.iocoder.boot.array.linkList.SingleLinkedList;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
 
 /**
  * @Description
@@ -13,27 +16,55 @@ import org.junit.Test;
 public class TestSingleLinkedList {
 
     @Test
-    public void test1(){
+    public void test1() {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         singleLinkedList.addFirst(1);
         singleLinkedList.addFirst(2);
         singleLinkedList.addFirst(3);
-        singleLinkedList.loop1(value->{
-            System.out.println(value);
+        singleLinkedList.loop1(value -> {
+//            System.out.println(value);
         });
-        singleLinkedList.loop2(value->{
-            System.out.println(value);
+        singleLinkedList.loop2(value -> {
+//            System.out.println(value);
         });
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         singleLinkedList.addFirst(1);
         singleLinkedList.addFirst(2);
         singleLinkedList.addFirst(3);
         for (Integer i : singleLinkedList) {
-            System.out.println(i);
+//            System.out.println(i);
         }
+    }
+
+    @Test
+    public void test3() {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+
+        singleLinkedList.addLast(1);
+        singleLinkedList.addLast(2);
+        Assertions.assertIterableEquals(List.of(1, 2), singleLinkedList);
+        for (Integer i : singleLinkedList) {
+//            System.out.println(i);
+        }
+
+    }
+
+    @Test
+    public void testGetNode() {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+
+        singleLinkedList.addLast(1);
+        singleLinkedList.addLast(2);
+        singleLinkedList.addLast(3);
+        int i = singleLinkedList.getNodeValue(1);
+
+        System.out.println(i);
+//        int j = singleLinkedList.getNodeValue(10);
+//        System.out.println(j);
     }
 
 }
